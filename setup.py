ac5cc4ceed84e092bfbfa0541e1ee29034c51c28
Tomasz Wysocki <tomasz@pozytywnie.pl>
2014-03-18 11:41:53 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python                                                                                                                                        
+from setuptools import setup, find_packages
+
+setup(version='0',
+      name="RouterOS-api",
+      description='Python API to RouterBoard devices produced by MikroTik.',
+      author='Tomasz Wysocki',
+      author_email='tomasz@pozytywnie.pl',
+      packages=find_packages(),
+      test_suite="tests",                          
+)
