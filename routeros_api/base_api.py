3f6240aaa33ea4d5fcf6da8057862b995e893d07
Tomasz Wysocki <tomasz@pozytywnie.pl>
2014-03-18 14:36:46 +0100

    Refactor.

--- routeros_api/base_api.py
+++ routeros_api/base_api.py
@@ -1,3 +1,12 @@
+LENGTH_MATRIX = [
+    (0x80, 0x0),
+    (0x40, 0x80),
+    (0x20, 0xC0),
+    (0x10, 0xE0),
+    (0x1, 0xF0),
+]
+
+
 def encode_length(length):
     data, number_of_bytes = _encode_length(length)
     return to_bytes(data, number_of_bytes)
@@ -6,14 +15,8 @@ def encode_length(length):
 def _encode_length(length):
     if length < 0:
         raise TypeError("Negative length.")
-    length_matrix = [
-        (0x80, 0x0),
-        (0x40, 0x80),
-        (0x20, 0xC0),
-        (0x10, 0xE0),
-        (0x1, 0xF0),
-    ]
-    for bytes, (max_value, mask) in enumerate(length_matrix):
+
+    for bytes, (max_value, mask) in enumerate(LENGTH_MATRIX):
         offset = 8 * bytes
         if length < (max_value << offset):
             return length | (mask << offset), bytes + 1
