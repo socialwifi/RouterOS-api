4697d251e6a621a49b9eb39237d5163691ec8c23
Jakub Skiepko <jakub@pozytywnie.pl>
2014-06-28 10:47:15 +0200

    Refactor.

--- routeros_api/exception_aware_api_communicator.py
+++ routeros_api/exception_aware_api_communicator.py
@@ -5,33 +5,25 @@ from routeros_api import exceptions
 class ExceptionAwareApiCommunicator(api_communicator.ApiCommunicator):
     def __init__(self, base):
         super(ExceptionAwareApiCommunicator, self).__init__(base)
-        self.exception_handler = ExceptionMultiHandler()
+        self.exception_handlers = []
 
     def send_command(self, command):
         try:
             super(ExceptionAwareApiCommunicator, self).send_command(command)
         except exceptions.RouterOsApiError as e:
-            self.exception_handler.handle(e)
+            self.handle_exception(e)
             raise
 
     def receive(self, tag):
         try:
             return super(ExceptionAwareApiCommunicator, self).receive(tag)
         except exceptions.RouterOsApiError as e:
-            self.exception_handler.handle(e)
+            self.handle_exception(e)
             raise
 
     def add_handler(self, handler):
-        self.exception_handler.add_handler(handler)
+        self.exception_handlers.append(handler)
 
-
-class ExceptionMultiHandler(object):
-    def __init__(self):
-        self.subhandlers = []
-
-    def add_handler(self, handler):
-        self.subhandlers.append(handler)
-
-    def handle(self, exception):
-        for subhandler in self.subhandlers:
+    def handle_exception(self, exception):
+        for subhandler in self.exception_handlers:
             subhandler.handle(exception)
