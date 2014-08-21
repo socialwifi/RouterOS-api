c696ed529bea2d787360a6dd16a19c080e0f7927
Jakub Skiepko <jakub@pozytywnie.pl>
2014-08-21 15:42:01 +0200

    Add more types implementation.

--- routeros_api/__init__.py
+++ routeros_api/__init__.py
@@ -1,4 +1,5 @@
 from routeros_api.api import connect
 from routeros_api import query
+from routeros_api import api_structure
 
-__all__=['connect', query]
+__all__=['connect', query, api_structure]
--- routeros_api/api_structure.py
+++ routeros_api/api_structure.py
@@ -8,4 +8,22 @@ class StringField(object):
     def get_python_value(self, bytes):
         return bytes.decode()
 
+
+class BytesField:
+    def get_mikrotik_value(self, bytes):
+        return bytes
+
+    def get_python_value(self, bytes):
+        return bytes
+
+
+class BooleanField:
+    def get_mikrotik_value(self, condition):
+        return b'yes' if condition else b'no'
+
+    def get_python_value(self, bytes):
+        assert bytes in (b'yes', b'true', b'no', b'false')
+        return bytes in (b'yes', b'true')
+
+
 default_structure = collections.defaultdict(StringField)
