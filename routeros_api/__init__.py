1299781eb893fbca3a783d65603f1345458bf23e
Jakub Skiepko <jakub@pozytywnie.pl>
2014-08-21 15:45:05 +0200

    Fix __all__.

--- routeros_api/__init__.py
+++ routeros_api/__init__.py
@@ -2,4 +2,4 @@ from routeros_api.api import connect
 from routeros_api import query
 from routeros_api import api_structure
 
-__all__=['connect', query, api_structure]
+__all__ = ['connect', 'query', 'api_structure']
