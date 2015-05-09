851ceea265105123a85f29a7f5bfc5cd2be8b6ba
Jakub Skiepko <jakub@pozytywnie.pl>
2015-05-09 16:36:14 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python                                                                                                                                        
 from setuptools import setup, find_packages
 
-setup(version='0.11',
+setup(version='0.12',
       name="RouterOS-api",
       description='Python API to RouterBoard devices produced by MikroTik.',
       author='Tomasz Wysocki',
