c66d8d2e709f559af242172cd7ed596e62586bc5
Jakub Gocławski <jakub@goclawski.net>
2018-08-23 14:30:22 +0200

    Prepare for automated release.

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+Changelog for RouterOS-api
+==========================
+
+0.15 (unreleased)
+------------------
+
+- Support new login API for v6.43. ([#29](https://github.com/socialwifi/RouterOS-api/issues/29))
+  
+  **Backwards-incompatible**: use `plaintext_login=True` if you connect with devices with OS older than v6.43.
+
+- Add SSL encryption. ([#30](https://github.com/socialwifi/RouterOS-api/issues/30))
+
+
+0.14 (2016-04-15)
+------------------
+
+- Beginning of tracking changelog.
--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python                                                                                                                                        
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-setup(version='0.14',
-      name="RouterOS-api",
-      description='Python API to RouterBoard devices produced by MikroTik.',
-      author='Tomasz Wysocki',
-      author_email='tomasz@pozytywnie.pl',
-      packages=find_packages(),
-      test_suite="tests",
-      license="MIT",
-      install_requires=['six'],
+
+setup(
+    name="RouterOS-api",
+    version='0.14.dev0',
+    description='Python API to RouterBoard devices produced by MikroTik.',
+    author='Social WiFi',
+    author_email='it@socialwifi.com',
+    url='https://github.com/socialwifi/RouterOS-api',
+    packages=find_packages(),
+    test_suite="tests",
+    license="MIT",
+    install_requires=['six'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
