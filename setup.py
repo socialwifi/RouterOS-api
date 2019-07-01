ff3f4272d65e99d12a5256a370d2ac7dfc9cddbc
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2019-07-01 11:49:24 +0200

    Fix: add ipaddress requirement to python older than 3.3

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python                                                                                                                                        
+import sys
+
 from setuptools import setup, find_packages
+
+requirements = ['six']
+
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 3):
+    requirements.append('ipaddress')
 
 setup(version='0.14',
       name="RouterOS-api",
@@ -9,5 +16,4 @@ setup(version='0.14',
       packages=find_packages(),
       test_suite="tests",
       license="MIT",
-      install_requires=['six'],
-)
+      install_requires=requirements)
