67790f3377edfa77677d4be7c7e46ebbf45a9f38
Mateusz Legięcki <mateusz.legiecki@socialwifi.com>
2019-09-24 14:28:32 +0200

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+def get_long_description():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
 setup(
     name="RouterOS-api",
     version='0.15.1.dev0',
     description='Python API to RouterBoard devices produced by MikroTik.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     author='Social WiFi',
     author_email='it@socialwifi.com',
     url='https://github.com/socialwifi/RouterOS-api',
